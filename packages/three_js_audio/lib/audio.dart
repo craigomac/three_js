77258fc4266d155814077a73b9274312b4c8fa13
Craig McMahon <craig@trainchinese.com>
2025-06-18 11:52:18 +0200

    Remove audioplayers

--- packages/three_js_audio/lib/audio.dart
+++ packages/three_js_audio/lib/audio.dart
@@ -1,19 +1,15 @@
 import 'dart:async';
-import 'package:audioplayers/audioplayers.dart';
 import 'package:three_js_core/three_js_core.dart';
 
 /// This utility class holds static references to some global audio objects.
 ///
 /// You can use as a helper to very simply play a sound or a background music.
 /// Alternatively you can create your own instances and control them yourself.
-class Audio extends Object3D{
+class Audio extends Object3D {
   bool autoplay;
   bool loop;
   bool hasPlaybackControl;
   bool _isPlaying = false;
-
-  AudioPlayer? source;
-  //Uint8List? _buffer;
 
   int loopEnd = 0;
   int loopStart = 0;
@@ -28,19 +24,18 @@ class Audio extends Object3D{
 
   String path;
 
-  Audio({
-    required this.path,
-    double balance = 0.0,
-    double volume = 1.0,
-    this.playbackRate = 1.0,
-    this.hasPlaybackControl = true,
-    this.autoplay = false,
-    this.loop = false
-  }){
+  Audio(
+      {required this.path,
+      double balance = 0.0,
+      double volume = 1.0,
+      this.playbackRate = 1.0,
+      this.hasPlaybackControl = true,
+      this.autoplay = false,
+      this.loop = false}) {
     _balance = balance;
     _volume = volume;
 
-    if(autoplay){
+    if (autoplay) {
       play();
     }
   }
@@ -50,159 +45,116 @@ class Audio extends Object3D{
   // }
 
   @override
-  void dispose(){
+  void dispose() {
     _delay?.cancel();
-    source?.dispose();
     super.dispose();
   }
 
   /// Plays a single run of the given [file], with a given [volume].
-  Future<void> play([int delay = 0]) async{
-		if (_isPlaying) {
-			console.warning( 'Audio: Audio is already playing.' );
-			return;
-		}
+  Future<void> play([int delay = 0]) async {
+    if (_isPlaying) {
+      console.warning('Audio: Audio is already playing.');
+      return;
+    }
 
-		if(!hasPlaybackControl){
-			console.warning( 'Audio: this Audio has no playback control.' );
-			return;
-		}
+    if (!hasPlaybackControl) {
+      console.warning('Audio: this Audio has no playback control.');
+      return;
+    }
 
     _isPlaying = true;
-
-    if(source == null){
-      if(delay != 0){
-        _delay = Timer(Duration(milliseconds: delay), (){
-          _play();
-          _delay?.cancel();
-          _delay = null;
-        });
-      }
-      else{
-        _play();
-      }
-    }
-    else{
-      resume();
-    }
   }
 
   /// Plays a single run of the given [file], with a given [volume].
-  Future<void> _play() async{
-    final src = AudioPlayer();
-    src.onPlayerComplete.listen((event) {
-      _isPlaying = false;
-    });
-    //await src.setReleaseMode(loop?ReleaseMode.loop:ReleaseMode.stop);
-    //await src.setPlaybackRate(playbackRate);
-    await src.play(
-      AssetSource(path),
-      volume: _volume,
-      mode: PlayerMode.lowLatency,
-      position: Duration(milliseconds: loopStart),
-      balance: _balance
-    );
-    
-    source = src;
-  }
+  Future<void> _play() async {}
 
   /// Stops the currently playing background music track (if any).
   Future<void> stop() async {
-		if (!hasPlaybackControl) {
-			console.warning( 'Audio: this Audio has no playback control.' );
-			return;
-		}
+    if (!hasPlaybackControl) {
+      console.warning('Audio: this Audio has no playback control.');
+      return;
+    }
 
     _delay?.cancel();
     _delay = null;
     _isPlaying = false;
-    await source?.stop();
   }
 
   /// Resumes the currently played (but resumed) background music.
   Future<void> resume() async {
     _isPlaying = true;
-    await source?.resume();
   }
 
   /// Pauses the background music without unloading or resetting the audio
   /// player.
   Future<void> pause() async {
-		if (!hasPlaybackControl) {
-			console.warning( 'Audio: this Audio has no playback control.' );
-			return;
-		}
+    if (!hasPlaybackControl) {
+      console.warning('Audio: this Audio has no playback control.');
+      return;
+    }
 
-		if(_isPlaying) {
+    if (_isPlaying) {
       _isPlaying = false;
-      await source?.pause();
     }
 
     _delay?.cancel();
     _delay = null;
   }
 
-	double? getPlaybackRate() {
-		return source?.playbackRate;
-	}
+  double? getPlaybackRate() {
+    return 1.0;
+  }
 
-	Future<void>? setPlaybackRate(double value) async{
-		if (!hasPlaybackControl) {
-			console.warning( 'Audio: this Audio has no playback control.' );
-			return;
-		}
+  Future<void>? setPlaybackRate(double value) async {
+    if (!hasPlaybackControl) {
+      console.warning('Audio: this Audio has no playback control.');
+      return;
+    }
 
-		if (_isPlaying) {
+    if (_isPlaying) {
       playbackRate = value;
-			await source?.setPlaybackRate(value);
-		}
-	}
+    }
+  }
 
-	bool getLoop() {
-		if (!hasPlaybackControl ) {
-			console.warning( 'Audio: this Audio has no playback control.' );
-			return false;
-		}
+  bool getLoop() {
+    if (!hasPlaybackControl) {
+      console.warning('Audio: this Audio has no playback control.');
+      return false;
+    }
 
-		return source?.releaseMode == ReleaseMode.loop;
-	}
+    return false;
+  }
 
-	Future<void> setLoop( value ) async{
-		if (!hasPlaybackControl) {
-			console.warning( 'Audio: this Audio has no playback control.' );
-			return;
-		}
+  Future<void> setLoop(value) async {
+    if (!hasPlaybackControl) {
+      console.warning('Audio: this Audio has no playback control.');
+      return;
+    }
 
-		loop = value;
+    loop = value;
+  }
 
-		if (_isPlaying ) {
-			await source?.setReleaseMode(loop?ReleaseMode.loop:ReleaseMode.stop);
-		}
-	}
+  void setLoopStart(int value) {
+    loopStart = value;
+  }
 
-	void setLoopStart(int value ) {
-		loopStart = value;
-	}
+  void setLoopEnd(int value) {
+    loopEnd = value;
+  }
 
-	void setLoopEnd(int value ) {
-		loopEnd = value;
-	}
+  double? getBalance() {
+    return _balance;
+  }
 
-	double? getBalance() {
-		return source?.balance;
-	}
-
-	Future<void> setBalance(double value ) async{
+  Future<void> setBalance(double value) async {
     _balance = value;
-		await source?.setBalance(value);
-	}
+  }
 
-	double? getVolume() {
-		return source?.volume;
-	}
+  double? getVolume() {
+    return _volume;
+  }
 
-	Future<void> setVolume(double value ) async{
+  Future<void> setVolume(double value) async {
     _volume = value;
-		await source?.setVolume(value);
-	}
+  }
 }
